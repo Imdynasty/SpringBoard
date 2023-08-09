62779fa6af7946a744711988937e01bd77fb83fa
Imdynasty <osm05230@gmail.com>
2023-08-09 18:12:19 +0900

    0809

--- src/main/java/com/foodrecommend/configs/SecurityConfig.java
+++ src/main/java/com/foodrecommend/configs/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.foodrecommend.configs;
 
+import com.foodrecommend.models.member.LoginFailureHandler;
+import com.foodrecommend.models.member.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,11 +9,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.formLogin()
+                .loginPage("/member/login")
+                .usernameParameter("userId")
+                .passwordParameter("userPw")
+                .successHandler(new LoginSuccessHandler())
+                .failureHandler(new LoginFailureHandler())
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                .logoutSuccessUrl("/member/login");
+
+
         return http.build();
     }
     @Bean
