693753f6673f1b55da9a4e62239b1344e8754731
Imdynasty <osm05230@gmail.com>
2023-08-10 21:34:32 +0900

    0810

---
+++ src/main/java/com/foodrecommend/restcontrollers/CommonRestController.java
@@ -0,0 +1,7 @@
+package com.foodrecommend.restcontrollers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("com.foodrecommend.restcontrollers")
+public class CommonRestController {
+}
