c93b51ee72d281056c69d4d5956b850429fa224e
Imdynasty <osm05230@gmail.com>
2023-08-17 20:17:59 +0900

    0817 4일차 1번째 동영상시청

--- src/main/java/com/foodrecommend/controllers/CommonController.java
+++ src/main/java/com/foodrecommend/controllers/CommonController.java
@@ -1,16 +1,17 @@
 package com.foodrecommend.controllers;
 
 import com.foodrecommend.commons.CommonException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice("com.foodrecommend.controllers")
+@ControllerAdvice("org.koreait.controllers")
 public class CommonController {
 
     @ExceptionHandler(Exception.class)
-    public String errorHandler(Exception e, Model model, HttpServletResponse response) {
+    public String errorHandler(Exception e, Model model, HttpServletRequest request, HttpServletResponse response) {
 
         int status = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
         if (e instanceof CommonException) {
@@ -19,10 +20,14 @@ public class CommonController {
         }
 
         response.setStatus(status);
+        String URL = request.getRequestURI();
 
         model.addAttribute("status", status);
+        model.addAttribute("path", URL);
         model.addAttribute("message", e.getMessage());
         model.addAttribute("exception", e);
+
+        e.printStackTrace();
 
         return "error/common";
     }
